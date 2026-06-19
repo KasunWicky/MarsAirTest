1439a04dd44b684458d75e6dc64a0aab6a63885d
Kasun Wickramarathna <kasun.umail@gmail.com>
2026-06-19 16:21:04 +0800

    Added SL4J logging config

--- src/main/java/org/pracTest/TestCofig.java
+++ src/main/java/org/pracTest/TestCofig.java
@@ -1,11 +1,13 @@
 package org.pracTest;
 
 import config.ConfigManager;
+import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Test;
-
+@Slf4j
 public class TestCofig {
     @Test
     public void testing1() {
+        log.info("Test");
         String browser = ConfigManager.get("browser");
         System.out.println(browser);
     }
