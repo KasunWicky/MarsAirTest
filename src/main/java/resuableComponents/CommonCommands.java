a19aca7aab8f0927aa09b2e9daceadbdef74bf83
Kasun Wickramarathna <kasun.umail@gmail.com>
2026-06-21 02:44:23 +0800

    Added type command

--- src/main/java/resuableComponents/CommonCommands.java
+++ src/main/java/resuableComponents/CommonCommands.java
@@ -1,14 +1,19 @@
 package resuableComponents;
 
 import config.EnvConfig;
+import driver.Driver;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
+
 import java.time.Duration;
+
 import runner.ScreenshotListener;
 import org.testng.annotations.Listeners;
+
 
 @Listeners(ScreenshotListener.class)
 public class CommonCommands {
@@ -20,6 +25,8 @@ public class CommonCommands {
      * @param element
      */
     public static void click(WebElement element) {
+        until(element, Until.VISIBLE);
+        until(element, Until.CLICKABLE);
         element.click();
     }
 
@@ -29,14 +36,53 @@ public class CommonCommands {
      * @param selectText
      */
     public static void selectDropDown(WebElement element, String selectText) {
+        until(element, Until.VISIBLE);
+        until(element, Until.ENABLED);
         Select dropDown = new Select(element);
         dropDown.selectByVisibleText(selectText);
     }
 
-    public static FluentWait<WebDriver> wait(WebDriver driver) {
+    public static void type(WebElement element, String textToType) {
+        until(element, Until.VISIBLE);
+        until(element, Until.CLICKABLE);
+        element.clear();
+        element.sendKeys(textToType);
+    }
+
+    public enum Until {
+        VISIBLE,      // element is displayed on the page
+        CLICKABLE,    // element is displayed AND enabled
+        INVISIBLE,    // element is hidden or removed from DOM
+        ENABLED       // element is enabled (e.g. button not disabled)
+    }
+
+    public static WebElement until(WebElement element, Until condition) {
+        FluentWait<WebDriver> wait = buildWait(Driver.getDriver());
+
+        switch (condition) {
+            case VISIBLE:
+                return wait.until(ExpectedConditions.visibilityOf(element));
+
+            case CLICKABLE:
+                return wait.until(ExpectedConditions.elementToBeClickable(element));
+
+            case INVISIBLE:
+                wait.until(ExpectedConditions.invisibilityOf(element));
+                return element;
+
+            case ENABLED:
+                return wait.until(driver -> element.isEnabled() ? element : null);
+
+            default:
+                throw new IllegalArgumentException("Unsupported condition: " + condition);
+        }
+    }
+
+    public static FluentWait<WebDriver> buildWait(WebDriver driver) {
         return new FluentWait<>(driver)
                 .withTimeout(Duration.ofSeconds(DEFAULT_TIMEOUT_SECONDS))    // Max wait time
                 .pollingEvery(Duration.ofMillis(DEFAULT_POLLING_MS))   // Frequency of DOM checks
                 .ignoring(NoSuchElementException.class);// Disregard this error during loops
     }
+
 }
