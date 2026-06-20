b3792b7e2cf7474efa0e67782f274dda536b1a96
Kasun Wickramarathna <kasun.umail@gmail.com>
2026-06-20 12:23:32 +0800

    Added data driven

--- src/main/java/org/pracTest/TestHomePage.java
+++ src/main/java/org/pracTest/TestHomePage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import webElements.MainPage;
 import webElements.SearchResultPage;
@@ -14,6 +15,14 @@ import webElements.SearchResultPage;
 @Slf4j
 public class TestHomePage {
     WebDriver driver;
+
+    @DataProvider(name = "flightScheduleData")
+    private Object[][] flightScheduleData() {
+        return new Object[][]{
+                {"Select...", "Select...", "Unfortunately, this schedule is not possible. Please try again."},
+                {"July", "July", "Unfortunately, this schedule is not possible. Please try again."}
+        };
+    }
 
     @BeforeClass
     public void setupInvocation() {
@@ -27,26 +36,28 @@ public class TestHomePage {
         Assert.assertEquals(actualTitle, "Mars Airlines: Home");
     }
 
-    @Test(dependsOnMethods = "verifyingHomePage")
-    public void bookFlight() {
+    @Test(dependsOnMethods = "verifyingHomePage", dataProvider = "flightScheduleData")
+    public void bookFlight(String departing, String returning, String expectedResults) {
         MainPage mainPage = new MainPage(driver);
 
         Select departingDropDown = new Select(mainPage.dropdown_departing());
-        departingDropDown.selectByVisibleText("Select...");
+        departingDropDown.selectByVisibleText(departing);
         Select returningDropDown = new Select(mainPage.dropdown_returning());
-        returningDropDown.selectByVisibleText("Select...");
+        returningDropDown.selectByVisibleText(returning);
         mainPage.button_search().click();
         //wait for submission to load  for
+        verifySearchResults(expectedResults);
+
     }
 
-    @Test(dependsOnMethods = "bookFlight")
-    public void verifySearchResults() {
-        String expectedResult = "Unfortunately, this schedule is not possible. Please try again.";
+    // @Test(dependsOnMethods = "bookFlight")
+    protected void verifySearchResults(String expectedResult) {
+        //String expectedResult = "Unfortunately, this schedule is not possible. Please try again.";
         SearchResultPage searchResultPage = new SearchResultPage(driver);
         Assert.assertEquals(searchResultPage.mainHeader().getText(), "Search Results");
         Assert.assertEquals(searchResultPage.textbox_Result().getText(), expectedResult);
     }
-
+    public void bookFlight()
     @AfterClass
     public void quit() {
         log.info("Test is being closed");
